375ea2f98ed7664bed61c422f059cfacab6b9a06
paul.houlston <paul.houlston@hotmail.co.uk>
2017-02-25 17:58:25 +0000

    Made failing test pass

--- tests/Tests.cs
+++ tests/Tests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace Tests
 {
@@ -6,13 +7,27 @@ namespace Tests
     {
         public class WHEN_the_Bishop_is_in_position_E4
         {
+            readonly string[] validMoves = new[] { "A8", "B7", "C6", "D5", "B3", "A2", "B1", "C2", "D3", "F5", "G6", "H7" };
+
             [Fact]
             public void THEN_the_Bishop_can_move_to_any_diagonal_space_from_their_position()
             {
-                var moveIsValid= false;
+                var moveIsValid = false;
+
+                var bishop = new Bishop();
+
+                bishop.Move(() => moveIsValid = true);
 
                 Assert.True(moveIsValid);
             }
+        }
+    }
+    
+    public class Bishop
+    {
+        public void Move(Action onMoveValid)
+        {
+            onMoveValid();
         }
     }
 }
